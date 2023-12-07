1007974879d987d7ee93ddb2b912be97fb6b1e0f
hordunlarmy <hordunlarmy@gmail.com>
2023-12-07 17:10:05 +0000

    Add nginx configuration for serving static files

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,14 +3,11 @@
 apt-get update
 apt-get -y install nginx
 ufw allow 'Nginx HTTP'
-mkdir -p /data/
-mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-touch /data/web_static/releases/test/index.html
 echo "<h1>Hello World</h1>" > /data/web_static/releases/test/index.html
 ln -s -f /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i '/listen 80 default_server/a location /hbnb_static {/n/t/t alias /data/web_static/current/;/n}' /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server/a location /hbnb_static {/n/t/t alias /data/web_static/current/;/n}' /etc/nginx/sites-available/hordun.tech
 sudo service nginx restart
