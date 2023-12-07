3fe990f77ddd061eea37139046968d7fc55412e2
hordunlarmy <hordunlarmy@gmail.com>
2023-12-07 16:49:33 +0000

    Setup web servers and create necessary directories and files

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
-# sets up web servers for the deployment of web_static\
+# sets up web servers for the deployment of web_static
 apt-get update
 apt-get -y install nginx
 ufw allow 'Nginx HTTP'
+mkdir -p /data/
+mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
+touch /data/web_static/releases/test/index.html
 echo "<h1>Hello World</h1>" > /data/web_static/releases/test/index.html
 ln -s -f /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
