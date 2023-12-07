2d5fcd6fed1cd36ec8855f2e7cfadc344a4d0830
hordunlarmy <hordunlarmy@gmail.com>
2023-12-07 20:35:26 +0000

    Generate .tgz archive from web_static contents

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
+# a Fabric script
 from fabric.api import local
 from datetime import datetime
 import os
 
 
 def do_pack():
-    """
-    a Fabric script that generates a .tgz archive from the contents of
-    the web_static
-    """
+    """ generates a .tgz archive from the contents of the web_static """
     try:
         now = datetime.now()
         filename = "web_static_{}.tgz".format(
@@ -18,8 +16,7 @@ def do_pack():
         path = local(f"tar -cvzf versions/{filename} web_static")
         file_path = f"versions/{filename}"
         file_size = os.path.getsize(file_path)
-
         print(f"web_static packed: versions/{filename} -> {file_size}Bytes")
-        return path
+        return file_path
     except Exception as e:
         return None
