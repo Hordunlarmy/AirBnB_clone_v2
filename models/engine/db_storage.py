03338c11d56a1e713e4b9f8ebfc94483ed910237
hordunlarmy <hordunlarmy@gmail.com>
2023-12-28 08:07:38 +0000

    "Add close method to DBStorage and FileStorage"

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -73,3 +73,9 @@ class DBStorage:
         session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(session)
         self.__session.close()
+
+    def close(self):
+        """
+        call remove() method on the private session attribute
+        """
+        self.__session.remove()
