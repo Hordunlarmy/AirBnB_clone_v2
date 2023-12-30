d43890335e68753a1cfc9e27e29095f6cd56fece
hordunlarmy <hordunlarmy@gmail.com>
2023-12-30 07:47:23 +0000

    Fix typo in 7-states_list.html and add debug mode to app.run

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -76,7 +76,14 @@ def get_states():
     List states
     """
     states = storage.all()
+    print(states)
     return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Method to remove current SQLAlchemy Session"""
+    storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
@@ -86,11 +93,5 @@ def states_and_cities():
     return render_template('8-cities_by_states.html', states=states)
 
 
-@app.teardown_appcontext
-def teardown(self):
-    """Method to remove current SQLAlchemy Session"""
-    storage.close()
-
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=True)
